76084cd0cbc0b7ddf90de386a64ca7e412caa597
asher.lv <243485908@qq.com>
2019-05-05 22:06:54 +0800

    xiaohong shi zhu

--- leasing-applications-parent/leasing-applications-config/src/main/java/com/cloudkeeper/leasing/config/AccessTokenInterceptor.java
+++ leasing-applications-parent/leasing-applications-config/src/main/java/com/cloudkeeper/leasing/config/AccessTokenInterceptor.java
@@ -33,45 +33,45 @@ public class AccessTokenInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        //如果不是映射到方法直接通过
-        if (!(handler instanceof HandlerMethod)
-                || ((HandlerMethod) handler).getBean() instanceof ApiResourceController
-                || ((HandlerMethod) handler).getBean() instanceof Swagger2Controller) {
-            return true;
-        }
-        HandlerMethod handlerMethod = (HandlerMethod) handler;
-        Method method = handlerMethod.getMethod();
-        Authorization authorization = method.getAnnotation(Authorization.class);
-        if (authorization != null && !authorization.required()) {
-            //过滤不拦截的方法
-            return true;
-        }
-        //从header中得到token
-        String token = request.getHeader(AuthorizationConstants.AUTHORIZATION);
-        if (!StringUtils.hasText(token)) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return false;
-        }
-        Claims claims;
-        try {
-            claims = TokenUtil.parseJWT(token);
-        } catch (ExpiredJwtException expiredJwtException) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            log.warn("token 过期了");
-            return false;
-        } catch (Exception exception) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            log.warn("无效 token");
-            return false;
-        }
-        String principalId = claims.getId();
-        String token2 = redisTemplate.opsForValue().get(AuthorizationConstants.REDIS_TOKEN_KEY + principalId);
-        if (!token.equals(token2)) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            log.info("用户不存在或已失效，请重新登录");
-            return false;
-        }
-        request.getSession().setAttribute(AuthorizationConstants.CURRENT_USER_ID, principalId);
+//        //如果不是映射到方法直接通过
+//        if (!(handler instanceof HandlerMethod)
+//                || ((HandlerMethod) handler).getBean() instanceof ApiResourceController
+//                || ((HandlerMethod) handler).getBean() instanceof Swagger2Controller) {
+//            return true;
+//        }
+//        HandlerMethod handlerMethod = (HandlerMethod) handler;
+//        Method method = handlerMethod.getMethod();
+//        Authorization authorization = method.getAnnotation(Authorization.class);
+//        if (authorization != null && !authorization.required()) {
+//            //过滤不拦截的方法
+//            return true;
+//        }
+//        //从header中得到token
+//        String token = request.getHeader(AuthorizationConstants.AUTHORIZATION);
+//        if (!StringUtils.hasText(token)) {
+//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//            return false;
+//        }
+//        Claims claims;
+//        try {
+//            claims = TokenUtil.parseJWT(token);
+//        } catch (ExpiredJwtException expiredJwtException) {
+//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//            log.warn("token 过期了");
+//            return false;
+//        } catch (Exception exception) {
+//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//            log.warn("无效 token");
+//            return false;
+//        }
+//        String principalId = claims.getId();
+//        String token2 = redisTemplate.opsForValue().get(AuthorizationConstants.REDIS_TOKEN_KEY + principalId);
+//        if (!token.equals(token2)) {
+//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//            log.info("用户不存在或已失效，请重新登录");
+//            return false;
+//        }
+//        request.getSession().setAttribute(AuthorizationConstants.CURRENT_USER_ID, principalId);
         return true;
     }
 }
