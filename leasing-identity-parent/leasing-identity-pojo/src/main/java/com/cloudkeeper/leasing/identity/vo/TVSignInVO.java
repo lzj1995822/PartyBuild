bfd332502bc0f53a2ca305675ff5c58b6476bdeb
cqhssh <1170669705@qq.com>
2019-07-02 10:02:19 +0800

    远教电视端

--- leasing-identity-parent/leasing-identity-pojo/src/main/java/com/cloudkeeper/leasing/identity/vo/TVSignInVO.java
+++ leasing-identity-parent/leasing-identity-pojo/src/main/java/com/cloudkeeper/leasing/identity/vo/TVSignInVO.java
@@ -9,10 +9,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 /**
  * 远教视频签到记录 VO
  * @author cqh
@@ -33,16 +29,36 @@ public class TVSignInVO extends BaseVO {
     @ApiModelProperty(value = "组织id", position = 10, required = true)
     private String organizationId;
 
-    /** 名称 */
-    @ApiModelProperty(value = "名称", position = 10, required = true)
+    /** 视频Id */
+    @ApiModelProperty(value = "视频Id", position = 10, required = true)
     private String videoId;
 
-    /** 名称 */
-    @ApiModelProperty(value = "名称", position = 10, required = true)
+    /** 签到 */
+    @ApiModelProperty(value = "签到", position = 10, required = true)
     private String signInRecord;
 
-    /** 名称 */
-    @ApiModelProperty(value = "名称", position = 10, required = true)
+    /** 签退 */
+    @ApiModelProperty(value = "签退", position = 10, required = true)
     private String signOutRecord;
+
+    /** 是否已读 */
+    @ApiModelProperty(value = "是否已读", position = 10, required = true)
+    private Integer flag;
+
+    /** 视频名称 */
+    @ApiModelProperty(value = "视频名称", position = 10, required = true)
+    private String videoName;
+
+    /** 视频时长 */
+    @ApiModelProperty(value = "视频时长", position = 10, required = true)
+    private String lengthOfTime;
+
+    /** 视频封面 */
+    @ApiModelProperty(value = "视频封面", position = 10, required = true)
+    private String videoCover;
+
+    /** 视频地址 */
+    @ApiModelProperty(value = "视频地址", position = 10, required = true)
+    private String videoUrl;
 
 }
