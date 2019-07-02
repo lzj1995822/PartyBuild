a3967713a2930600c70799e612f5e2f41201d2b9
cqhssh <1170669705@qq.com>
2019-07-02 14:25:51 +0800

    远教电视端

--- leasing-identity-parent/leasing-identity-service/src/main/java/com/cloudkeeper/leasing/identity/service/impl/TVSignInServiceImpl.java
+++ leasing-identity-parent/leasing-identity-service/src/main/java/com/cloudkeeper/leasing/identity/service/impl/TVSignInServiceImpl.java
@@ -37,7 +37,7 @@ public class TVSignInServiceImpl extends BaseServiceImpl<TVSignIn> implements TV
     }
 
     @Override
-    public TVSignIn save(String id, Integer type, String username) {
+    public synchronized TVSignIn save(String id, Integer type, String username) {
         Optional<TVSignIn> byId = tVSignInRepository.findById(id);
         if(byId.isPresent()){
             TVSignIn tvSignIn = byId.get();
