a1ea154992d0d1c2036efc6c4d81423330bae6c6
zhengyao.lu <zhengyao.lu@qq.com>
2020-03-20 09:44:22 +0800

    修改村书记任期信息

---
+++ leasing-identity-parent/leasing-identity-pojo/src/main/java/com/cloudkeeper/leasing/identity/vo/CadresStatisticsVO.java
@@ -0,0 +1,13 @@
+package com.cloudkeeper.leasing.identity.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CadresStatisticsVO {
+
+    private String id;
+    private String name;
+    private Setter parentDistrictName;
+}
