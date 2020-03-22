eea8d67393936d164b9cb89c0f2f1b929f227a5c
zhengyao.lu <zhengyao.lu@qq.com>
2020-03-22 23:06:22 +0800

    修改完成状态

--- leasing-identity-parent/leasing-identity-service/src/main/java/com/cloudkeeper/leasing/identity/service/impl/CadreTaskObjectServiceImpl.java
+++ leasing-identity-parent/leasing-identity-service/src/main/java/com/cloudkeeper/leasing/identity/service/impl/CadreTaskObjectServiceImpl.java
@@ -4,12 +4,8 @@ import com.cloudkeeper.leasing.base.repository.BaseRepository;
 import com.cloudkeeper.leasing.base.service.impl.BaseServiceImpl;
 import com.cloudkeeper.leasing.identity.domain.CadreTask;
 import com.cloudkeeper.leasing.identity.domain.CadreTaskObject;
-import com.cloudkeeper.leasing.identity.domain.VillageCadres;
 import com.cloudkeeper.leasing.identity.repository.CadreTaskObjectRepository;
-import com.cloudkeeper.leasing.identity.repository.CadreTaskRepository;
-import com.cloudkeeper.leasing.identity.repository.VillageCadresRepository;
 import com.cloudkeeper.leasing.identity.service.CadreTaskObjectService;
-import com.cloudkeeper.leasing.identity.service.CadreTaskService;
 import com.cloudkeeper.leasing.identity.vo.FinishRatioVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +14,6 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * 村书记模块发布任务对象记录 service
@@ -31,7 +25,7 @@ public class CadreTaskObjectServiceImpl extends BaseServiceImpl<CadreTaskObject>
 
     /** 村书记模块发布任务对象记录 repository */
     private final CadreTaskObjectRepository cadreTaskObjectRepository;
-    
+
     @Override
     protected BaseRepository<CadreTaskObject> getBaseRepository() {
         return cadreTaskObjectRepository;
@@ -45,7 +39,7 @@ public class CadreTaskObjectServiceImpl extends BaseServiceImpl<CadreTaskObject>
                 .withMatcher("note", ExampleMatcher.GenericPropertyMatchers.contains())
                 .withMatcher("townName", ExampleMatcher.GenericPropertyMatchers.contains());
     }
-    
+
     @Override
     public CadreTaskObject updateStatusByTaskIdAndObjectId(String taskId, String objectId) {
         CadreTaskObject cadreTaskObject = cadreTaskObjectRepository.findByTaskIdAndAndObjectId(taskId, objectId);
@@ -53,7 +47,7 @@ public class CadreTaskObjectServiceImpl extends BaseServiceImpl<CadreTaskObject>
             return null;
         }
         // 更新镇进度
-        String sql = "select count(case WHEN vc.state = '4' THEN 1 ELSE NULL END) as finish, count(case WHEN vc.state != '4' THEN 1 ELSE NULL END) as unfinish, count(*) as total FROM village_cadres vc WHERE vc.parentDistrictId = '" + objectId + "'";
+        String sql = "select count(case WHEN vc.state = '3' THEN 1 ELSE NULL END) as finish, count(case WHEN vc.state != '3' THEN 1 ELSE NULL END) as unfinish, count(*) as total FROM village_cadres vc WHERE vc.parentDistrictId = '" + objectId + "'";
         FinishRatioVO finishRatioVO = findBySql(FinishRatioVO.class, sql);
         BigDecimal divide = new BigDecimal(finishRatioVO.getFinish()).divide(new BigDecimal(finishRatioVO.getTotal()), 2, BigDecimal.ROUND_FLOOR);
         if (divide.equals(BigDecimal.ONE)) {
@@ -62,7 +56,7 @@ public class CadreTaskObjectServiceImpl extends BaseServiceImpl<CadreTaskObject>
         cadreTaskObject.setCurrentPercent(divide.toString());
 
         // 更新总进度
-        String totalSql = "select count(case WHEN vc.state = '4' THEN 1 ELSE NULL END) as finish, count(case WHEN vc.state != '4' THEN 1 ELSE NULL END) as unfinish, count(*) as total FROM village_cadres vc WHERE vc.parentDistrictId like '01%'";
+        String totalSql = "select count(case WHEN vc.state = '3' THEN 1 ELSE NULL END) as finish, count(case WHEN vc.state != '3' THEN 1 ELSE NULL END) as unfinish, count(*) as total FROM village_cadres vc WHERE vc.parentDistrictId like '01%'";
         FinishRatioVO finishRatioVO1 = findBySql(FinishRatioVO.class, totalSql);
         BigDecimal divide1 = new BigDecimal(finishRatioVO1.getFinish()).divide(new BigDecimal(finishRatioVO1.getTotal()), 2, BigDecimal.ROUND_FLOOR);
         CadreTask cadreTask = cadreTaskObject.getCadreTask();
