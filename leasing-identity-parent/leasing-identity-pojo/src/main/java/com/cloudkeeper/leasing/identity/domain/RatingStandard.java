0a711b492963ca7c7af5f08d4c020e0eac91aeb5
asher <243485908@qq.com>
2020-04-01 20:59:47 +0800

    更新

--- leasing-identity-parent/leasing-identity-pojo/src/main/java/com/cloudkeeper/leasing/identity/domain/RatingStandard.java
+++ leasing-identity-parent/leasing-identity-pojo/src/main/java/com/cloudkeeper/leasing/identity/domain/RatingStandard.java
@@ -39,57 +39,57 @@ public class RatingStandard extends BaseEntity {
     /** 担任村书记时长 */
     @ApiModelProperty(value = "担任村书记时长", position = 10, required = true)
     @Column(length = 60)
-    private String workDuration;
+    private String workDuration = "0";
 
     /** 能力研判 */
     @ApiModelProperty(value = "能力研判", position = 10, required = true)
     @Column(length = 60)
-    private String abilityJudgement;
+    private String abilityJudgement = "0";
 
     /** 上年度专职村书记考核等次 */
     @ApiModelProperty(value = "上年度专职村书记考核等次", position = 10, required = true)
     @Column(length = 60)
-    private String lastGrade;
+    private String lastGrade = "0";
 
     /** 年度考核获得“称职”及以上等次的次数 */
     @ApiModelProperty(value = "年度考核获得“称职”及以上等次的次数", position = 10, required = true)
     @Column(length = 60)
-    private String gradeTimes;
+    private String gradeTimes = "0";
 
     /** 连续几年年度考核获得“称职”及以上等次 */
     @ApiModelProperty(value = "连续几年年度考核获得“称职”及以上等次", position = 10, required = true)
     @Column(length = 60)
-    private String gradeLastTimes;
+    private String gradeLastTimes = "0";
 
     /** 连续几年年度考核获得“优秀“等次 */
     @ApiModelProperty(value = "连续几年年度考核获得“优秀“等次", position = 10, required = true)
     @Column(length = 60)
-    private String aGradeLastTimes;
+    private String aGradeLastTimes = "0";
 
     /** 年度考核获得“优秀”等次的次数 */
     @ApiModelProperty(value = "年度考核获得“优秀”等次的次数", position = 10, required = true)
     @Column(length = 60)
-    private String aGradeTimes;
+    private String aGradeTimes = "0";
 
     /** 连续几次年度考核获得前10 */
     @ApiModelProperty(value = "连续几次年度考核获得前10", position = 10, required = true)
     @Column(length = 60)
-    private String topTenGradeLastTimes;
+    private String topTenGradeLastTimes = "0";
 
     /** 连续几次年度考核获得前5 */
     @ApiModelProperty(value = "连续几次年度考核获得前5", position = 10, required = true)
     @Column(length = 60)
-    private String topFiveGradeLastTimes;
+    private String topFiveGradeLastTimes = "0";
 
     /** 连续几次年度考核获得前3 */
     @ApiModelProperty(value = "连续几次年度考核获得前3", position = 10, required = true)
     @Column(length = 60)
-    private String topThreeGradeLastTimes;
+    private String topThreeGradeLastTimes = "0";
 
     /** 获得表彰类型 */
     @ApiModelProperty(value = "获得表彰类型", position = 10, required = true)
     @Column(length = 60)
-    private String honoursType;
+    private String honoursType = "";
 
     /** 村书记 */
     @ApiModelProperty(value = "村书记", position = 10, required = true)
